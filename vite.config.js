6f30a30056b85d5e43c4fe28a0f8b4e06d70982d
g-a-v-i-n <gvnatknsn@gmail.com>
2023-08-20 10:48:25 -0700

    upgrade typescript and fix alias location in tesconfig

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
-import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@lib': path.resolve(__dirname, './src/lib'),
-        '@state': path.resolve(__dirname, './src/state'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 })
